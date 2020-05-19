35eae8d1377be5eea7b8a1df5ba728540bbfd8d0
Renevatia <renevatia@gmail.com>
2020-05-18 17:50:11 -0700

    helloworld

--- answers/helloworld.cpp
+++ answers/helloworld.cpp
@@ -1,5 +1,6 @@
 // just a test file
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -7,8 +8,14 @@ using namespace std;
 int main()
 {
     int x = 0;
+    ofstream myfile;
     cout << "Hello World";
     cout << endl;
+    
+    myfile.open ("helloworld-output.txt");
+    myfile << "Hello World";
+    myfile.close();
+
     cin >> x;
     
     return 0;

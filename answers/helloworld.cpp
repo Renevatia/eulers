af15a979a60051516560a97cc824f6a530b3c036
Renevatia <renevatia@gmail.com>
2020-05-18 17:46:22 -0700

    helloworld

---
+++ answers/helloworld.cpp
@@ -0,0 +1,15 @@
+// just a test file
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    cout << "Hello World";
+    cout << endl;
+    cin >> x;
+    
+    return 0;
+}
